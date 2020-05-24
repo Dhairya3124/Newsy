a225297af43651686efbce0c3c4881c1019811f0
Dhairya Khanna <dk9582671597@gmail.com>
2020-05-25 00:29:01 +0530

    firebase setup

---
+++ Desktop/project/Newsy/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjn3y9_WRlPTUuurpPjVIKNWvoqqTo-mY",
+    authDomain: "newsy-de897.firebaseapp.com",
+    databaseURL: "https://newsy-de897.firebaseio.com",
+    projectId: "newsy-de897",
+    storageBucket: "newsy-de897.appspot.com",
+    messagingSenderId: "410248785396",
+    appId: "1:410248785396:web:702fef1c9cbd469418dda1"
+  };
+
+  export default firebaseConfig;
